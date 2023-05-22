553f0436f183bdaddaf6bd7f0543ddbd872b5d3a
sattorov <sattorov.ahu@gmail.com>
2023-05-22 15:58:29 +0500

    this commit 22.05.2023 update create User table

--- app.py
+++ app.py
@@ -1,23 +1,21 @@
 from aiogram import executor
-
+import filters
+import handlers
+import middlewares
 from loader import dp, db
-import middlewares, filters, handlers
 from utils.notify_admins import on_startup_notify
 from utils.set_bot_commands import set_default_commands
 
 
 async def on_startup(dispatcher):
-    # Birlamchi komandalar (/star va /help)
+    # Default commands (/star and /help)
     await set_default_commands(dispatcher)
-    
-    # Malumotlar bazasini yaratish:
-    try:
-        db.create_table_users()
-    except Exception as err:
-        print(err)
 
-    # Bot ishga tushgani haqida adminga xabar berish
+    # Notify the admin that the bot has started
     await on_startup_notify(dispatcher)
+
+    # Create a database:
+    db.create_table_users()
 
 
 if __name__ == '__main__':
